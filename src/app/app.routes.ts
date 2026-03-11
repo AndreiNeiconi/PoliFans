156b3bf8151fa60de9530f6aa98b7bebbf3988ea
Neiconi Andrei <a.neiconi19@gmail.com>
2026-03-12 01:22:53 +0200

    fix bug 404 when refresh

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,12 +10,12 @@ import { RegisterComponent } from './features/register/register.component';
 export const routes: Routes = [
   // Default to Feed when visiting the home page
   { path: '', redirectTo: 'messages', pathMatch: 'full' },
-  { path: 'feed', component: FeedComponent },
-  { path: 'discover', component: DiscoverComponent },
-  { path: 'messages', component: MessagingComponent },
-  { path: 'subscription', component: SubscriptionComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: '/feed', component: FeedComponent },
+  { path: '/discover', component: DiscoverComponent },
+  { path: '/messages', component: MessagingComponent },
+  { path: '/subscription', component: SubscriptionComponent },
+  { path: '/login', component: LoginComponent },
+  { path: '/register', component: RegisterComponent },
   // Wildcard for 404
   { path: '**', redirectTo: 'feed' },
 ];
