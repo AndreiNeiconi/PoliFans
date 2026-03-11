b3f83c9dbca3756c59cf07acc602e423bf4f1346
Neiconi Andrei <a.neiconi19@gmail.com>
2026-03-12 01:41:03 +0200

    Resolve bug broke on refresh,use the useHash metod

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { FeedComponent } from './features/feed/feed.component';
 import { DiscoverComponent } from './features/discover/discover.component';
@@ -6,16 +6,27 @@ import { MessagingComponent } from './features/messaging/messaging.component';
 import { SubscriptionComponent } from './features/subscription/subscription.component';
 import { LoginComponent } from './features/login/login.component';
 import { RegisterComponent } from './features/register/register.component';
+import { NgModel } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+
 
 export const routes: Routes = [
-  // Default to Feed when visiting the home page
   { path: '', redirectTo: 'messages', pathMatch: 'full' },
-  { path: '/feed', component: FeedComponent },
-  { path: '/discover', component: DiscoverComponent },
-  { path: '/messages', component: MessagingComponent },
-  { path: '/subscription', component: SubscriptionComponent },
-  { path: '/login', component: LoginComponent },
-  { path: '/register', component: RegisterComponent },
-  // Wildcard for 404
+  { path: 'feed', component: FeedComponent },
+  { path: 'discover', component: DiscoverComponent },
+  { path: 'messages', component: MessagingComponent },
+  { path: 'subscription', component: SubscriptionComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: '**', redirectTo: 'feed' },
 ];
+
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { useHash: true })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
