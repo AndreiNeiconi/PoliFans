5f9a03e054009f8482704098f54240db38a692c0
Andrei Neiconi <69247432+AndreiNeiconi@users.noreply.github.com>
2026-04-20 15:06:56 +0300

    create the post component

---
+++ src/app/features/post_component/post.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'app-post',
+    standalone:true,
+    templateUrls:'./post-component.html'
+    styleUrls:'./post-component.css',
+})
+export class Appapost(){
+    
+}
