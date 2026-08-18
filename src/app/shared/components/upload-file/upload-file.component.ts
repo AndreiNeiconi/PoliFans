2f36c717e53f0be0ef4b47a45b21c150354e3033
Andrei Neiconi <a.neiconi19@gmail.com>
2026-08-18 20:23:38 +0300

    Add UploadFileComponent with file upload functionality and routing

--- src/app/shared/components/upload-file/upload-file.component.ts
+++ src/app/shared/components/upload-file/upload-file.component.ts
@@ -1,11 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { FileUploadService } from '../../../../services/file-upload.service';
 
 @Component({
   selector: 'app-upload-file',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './upload-file.component.html',
   styleUrl: './upload-file.component.css'
 })
 export class UploadFileComponent {
+  fileName:string ='';
+  constructor(private http:HttpClient,private fileUploadService:FileUploadService){}
+  onFileSelected(event:any){
+    const file:File = event.target.files[0];
+
+    if(file){
+      this.fileName = file.name;
+
+      const formData = new FormData();
+      formData.append("thumbnail",file);
+      this.fileUploadService.uploadFile(file)
+
+    }
+
+    return 0;
+  }
 
 }
